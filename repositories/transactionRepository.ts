7e165ab9ea38899ba1d86907cfce7c05b473fb8d
nkmbxy <nakamon22547@gmail.com>
2026-05-16 02:02:11 +0700

    feat: implement useReducer for transaction state management and add abort signal support for fetch requests

--- repositories/transactionRepository.ts
+++ repositories/transactionRepository.ts
@@ -6,24 +6,28 @@ import {
 } from "@/types/transaction";
 
 export const transactionRepository = {
-  getTransactions: (params?: {
-    page?: number;
-    pageSize?: number;
-    txType?: string;
-  }) =>
+  getTransactions: (
+    params?: {
+      page?: number;
+      pageSize?: number;
+      txType?: string;
+    },
+    signal?: AbortSignal,
+  ) =>
     apiClient
-      .get<TransactionResponse>("/transactions", { params })
+      .get<TransactionResponse>("/transactions", { params, signal })
       .then((res) => res.data),
 
-  getSummary: (month: string, year: string) =>
+  getSummary: (month: string, year: string, signal?: AbortSignal) =>
     apiClient
       .get<SummaryResponse>("/transactions/summary", {
         params: { month, year },
+        signal,
       })
       .then((res) => res.data),
 
-  getTransactionById: (uuid: string) =>
+  getTransactionById: (uuid: string, signal?: AbortSignal) =>
     apiClient
-      .get<TransactionDetailResponse>(`/transactions/${uuid}`)
+      .get<TransactionDetailResponse>(`/transactions/${uuid}`, { signal })
       .then((res) => res.data),
 };
